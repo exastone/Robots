af7b2da4190b16a313b95e13e3aef747ae6e4fa0
Andrew <adrwstn@gmail.com>
2023-06-11 05:45:00 -0230

    additional functions moved to Helpers class

--- src/Helpers.java
+++ src/Helpers.java
@@ -6,27 +6,59 @@ public class Helpers {
 
 	// Provided a 2D grid, prints the grid to the console
 	public static void printGrid(List<List<Character>> grid) {
-		for (int i = 0; i < grid.size(); i++) {
-			System.out.print(" " + i);
-			if (i == grid.size() - 1) {
-				System.out.println(" \t-> y");
+		int size = grid.size();
+
+		// Print column numbers
+		System.out.print("   ");
+		for (int i = 0; i < size; i++) {
+			if (i < 10)
+				System.out.print("| " + i + " ");
+			else
+				System.out.print("|" + i + " ");
+		}
+		System.out.println("\t-> y");
+
+		// Print grid rows
+		for (int rowIdx = 0; rowIdx < size; rowIdx++) {
+			List<Character> row = grid.get(rowIdx);
+
+			// Print row contents
+			if (rowIdx < 10)
+				System.out.print(rowIdx + "  ");
+			else
+				System.out.print(rowIdx + " ");
+			for (char cell : row) {
+				System.out.print("| " + cell + " ");
+			}
+			System.out.println("|");
+		}
+
+		// Print x-axis indicator
+		System.out.println("x↓\n");
+	}
+
+	// function to print the current position of robots in the room;
+	public static void printRobots() {
+		List<Robot> robots = RobotVacuumSimulator.getInstance().getRobots();
+		// note: (y,x) === (horizontal, vertical)
+		robots.forEach(robot -> System.out.println("Robot (" + robot.y + "," + robot.x + ") facing " + robot.getDirection()));
+	}
+
+	// Return a copy of the grid populated with robots at their current positions
+	public static List<List<Character>> returnRobotsInGrid(List<List<Character>> grid) {
+		List<Robot> robots = RobotVacuumSimulator.getInstance().getRobots();
+		robots.forEach(robot -> grid.get(robot.x).set(robot.y, robot.getDirection()));
+		return grid;
+	}
+
+	// Check if all tiles in grid have same character
+	public static boolean checkGrid(List<List<Character>> grid, char c) {
+		for (List<Character> row : grid) {
+			for (char cell : row) {
+				if (cell != c) {return false;}
 			}
 		}
-		IntStream.range(0, grid.size()).forEach(rowIdx -> {
-			List<Character> row = grid.get(rowIdx);
-			IntStream.range(0, row.size()).forEach(colIdx -> {
-				char cell = row.get(colIdx);
-				if (colIdx == row.size() - 1) {
-					System.out.print("|" + cell + "| " + rowIdx);
-					if (rowIdx == 0) {
-						System.out.print(" ↓ x");
-					}
-				} else {
-					System.out.print("|" + cell);
-				}
-			});
-			System.out.println();
-		});
+		return true;
 	}
 
 	// Defined to reduce code duplication
@@ -34,20 +66,7 @@ public class Helpers {
 		System.out.println("[Room View]");
 		Helpers.printGrid(grid);
 		System.out.println("[Room View w/ robots]");
-		Helpers.printGrid(RobotVacuumSimulator.getInstance().returnRobotsInGrid(grid));
-	}
-
-	// Check if all tiles in grid have same character
-	public static boolean checkGrid(List<List<Character>> grid, char c) {
-		int size = grid.size();
-		for (int i = 0; i < size - 1; i++) {
-			for (int j = 0; j < size - 1; j++) {
-				if (grid.get(i).get(j) != c) {
-					return false;
-				}
-			}
-		}
-		return true;
+		Helpers.printGrid(returnRobotsInGrid(grid));
 	}
 
 }
