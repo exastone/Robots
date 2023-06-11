ffb7a637729f6ec5fa6c2bab56a4921dcb3db6b6
Andrew <adrwstn@gmail.com>
2023-06-11 05:46:41 -0230

    Measure of program execution added to Main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 	public static void main(String[] args) {
 		RobotVacuumSimulator simulator = RobotVacuumSimulator.getInstance();
 		simulator.init();
+
+		long startTime = System.nanoTime();
 		simulator.simulate();
+		long endTime = System.nanoTime();
+
+		long elapsedTimeMs = (endTime - startTime) / 1_000_000;
+		System.out.println("Execution Time: " + elapsedTimeMs + " ms");
 	}
 }
